b7566bd4d9e47fa020e34240d48d085ced2c0e82
Whitney <designedbywh@gmail.com>
2023-02-28 17:02:44 -0500

    refactor name change

--- src/helpers/formatNumber.js
+++ src/helpers/formatNumber.js
@@ -1,8 +1,8 @@
 export default function formatNumber(currency) {
-  const formattedNumber2 = new Intl.NumberFormat("en-US", {
+  const formattedNumber = new Intl.NumberFormat("en-US", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(currency);
 
-  return formattedNumber2;
+  return formattedNumber;
 }
