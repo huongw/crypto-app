6424bc413846b4998c3541a965a0c4f195046431
Whitney <designedbywh@gmail.com>
2023-03-15 15:39:01 -0400

    add cleanup func to home

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,6 +11,8 @@ const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    const cancelToken = axios.CancelToken.source();
+
     const coinsURL = axios.get(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false"
     );
@@ -20,6 +22,7 @@ const Home = () => {
 
     axios
       .all([coinsURL, trendingURL], {
+        cancelToken: cancelToken.token,
         headers: {
           "Content-Type": "application/json",
         },
@@ -31,12 +34,16 @@ const Home = () => {
         })
       )
       .catch((err) => {
+        if (axios.isCancel(err)) return;
+
         console.log(err.message);
         setError(true);
       })
       .finally(() => {
         setIsLoading(false);
       });
+
+    return () => cancelToken.cancel();
   }, []);
 
   if (isLoading) return <Loader />;
