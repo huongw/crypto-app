83845b31a0023be28bc6e034e464f92965f4242a
Whitney <designedbywh@gmail.com>
2023-03-27 16:33:38 -0400

    Revert "Revert "implement useReducer to reduce error and isLoading states""
    This reverts commit ea2f026befb960bf48af0bc7c6ec0f7edae26e2c.

--- src/pages/CoinsPage/CoinsPage.jsx
+++ src/pages/CoinsPage/CoinsPage.jsx
@@ -15,7 +15,7 @@ const CoinsPage = () => {
 
   if (isLoading) return <Loader />;
 
-  if (error) return <Error message={error} />;
+  if (error) return <Error />;
 
   return (
     <div className="container">
