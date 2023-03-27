56aba5388d98dde057db481f067e1527519774e1
Whitney <designedbywh@gmail.com>
2023-03-27 16:33:51 -0400

    Revert "add background color to home page and images for about and hero sections"
    This reverts commit 9406ff1ac51e0eb048deb63f6889167b3e4b7df9.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -18,7 +18,7 @@ const NavBar = () => {
           </NavLink>
         </li>
         <li>
-          <NavLink to={"/coins"}>Discover</NavLink>
+          <NavLink to={"/coins"}>Coins</NavLink>
         </li>
         <li>
           <NavLink to={"/news"}>Crypto News</NavLink>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
-import { About, Discover, Hero, Trending } from "..";
+import { Link } from "react-router-dom";
+import { Trending } from "..";
 import "./HomePage.css";
 import { Error, Loader } from "../";
+import { BsArrowUpRight, BsArrowDownLeft } from "react-icons/bs";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -44,10 +46,55 @@ const Home = () => {
   if (error) return <Error message={error} />;
 
   return (
-    <div className="home-page">
-      <Hero />
-      <About />
-      <Discover coins={coins} />
+    <div>
+      <div className="hero-section">
+        <h1>
+          Welcome to <span className="logo-color">Crypto</span> Hive!
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos
+          repudiandae ullam voluptatibus ad incidunt quo magni tempore illo
+          beatae officia inventore ducimus sapiente numquam, ex eveniet harum
+          amet. Asperiores, harum?
+        </p>
+      </div>
+      <div className="about-section">about</div>
+      <div className="about-coins-section">
+        <div className="about-coins-wrapper">
+          <h2>
+            Explore Top Crypto-currencies like: <br />{" "}
+            <span className="bitcoin">Bitcoin</span>,{" "}
+            <span className="ethereum">Ethereum</span>, and{" "}
+            <span className="tether">Tether</span>
+          </h2>
+          <Link to={"/coins"}>See More Coins</Link>
+        </div>
+        <div className="coin-wrapper">
+          {coins.map((coin) => (
+            <div className="coin-row" key={coin.id}>
+              <div className="img-wrapper">
+                <img src={coin.image} alt={coin.name} />
+              </div>
+              <h4>{coin.name}</h4>
+              <p>${coin.current_price?.toLocaleString()}</p>
+              <p
+                className={
+                  coin.market_cap_change_percentage_24h < 0
+                    ? "negative percentage coin"
+                    : "percentage coin"
+                }
+              >
+                {coin.market_cap_change_percentage_24h < 0 ? (
+                  <BsArrowDownLeft />
+                ) : (
+                  <BsArrowUpRight />
+                )}{" "}
+                {coin.market_cap_change_percentage_24h?.toFixed(1)}%
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
       <Trending trending={trending} />
     </div>
   );
